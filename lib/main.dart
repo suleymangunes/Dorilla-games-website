50a64070fbb5881751b83c93483ced05a4664b44
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-06-05 11:59:04 +0300

    Animated background with Flare

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,118 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ResponsiveWebPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ResponsiveWebPage extends StatelessWidget {
+  const ResponsiveWebPage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              if (constraints.maxWidth > 800) {
+                // Büyük ekranlarda farklı bir görünüm
+                return const DesktopView();
+              } else {
+                // Mobil cihazlarda farklı bir görünüm
+                return const MobileView();
+              }
+            },
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class DesktopView extends StatelessWidget {
+  const DesktopView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return
+        // Container(
+        //   padding: const EdgeInsets.symmetric(horizontal: 100.0),
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       const Text(
+        //         'Welcome to my Responsive Web Page!',
+        //         style: TextStyle(fontSize: 24.0),
+        //         textAlign: TextAlign.center,
+        //       ),
+        //       const SizedBox(height: 20.0),
+        Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: const FlareActor(
+            "assets/animation/background.flr",
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+            animation: "midnight",
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
+    //       Image.asset(
+    //         'assets/logo/logo.png',
+    //         width: 400.0,
+    //       ),
+    //       const SizedBox(height: 20.0),
+    //       const Text(
+    //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam mauris vel lacus rhoncus, ac mattis lectus pulvinar. Quisque in risus nec mi mollis ultrices. Nullam efficitur erat id justo tristique, in dignissim est rhoncus. Sed mattis vestibulum nisl, a cursus erat fringilla nec.',
+    //         textAlign: TextAlign.center,
+    //         style: TextStyle(fontSize: 16.0),
+    //       ),
+    //     ],
+    //   ),
+    // );
+  }
+}
+
+class MobileView extends StatelessWidget {
+  const MobileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+
+        // Container(
+        //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: const [
+        //       Text(
+        //         'Welcome to my Responsive Web Page!',
+        //         style: TextStyle(fontSize: 18.0),
+        //         textAlign: TextAlign.center,
+        //       ),
+        //       SizedBox(height: 10.0),
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: const FlareActor("assets/animation/background.flr",
+                alignment: Alignment.center, fit: BoxFit.cover, animation: "midnight"));
+    // Image.asset(
+    //   'assets/logo/logo.png',
+    //   width: MediaQuery.of(context).size.width * 0.6,
+    // ),
+    //     SizedBox(height: 10.0),
+    //     Text(
+    //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam mauris vel lacus rhoncus, ac mattis lectus pulvinar. Quisque in risus nec mi mollis ultrices. Nullam efficitur erat id justo tristique, in dignissim est rhoncus. Sed mattis vestibulum nisl, a cursus erat fringilla nec.',
+    //       textAlign: TextAlign.center,
+    //       style: TextStyle(fontSize: 14.0),
+    //     ),
+    //   ],
+    // ),
+    // );
   }
 }
